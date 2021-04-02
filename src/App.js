d1cb2405192b5620c950bd919d01abef1b267ce1
Abdisamad Hussein <78183395+abdi-hu@users.noreply.github.com>
2021-04-02 16:12:31 -0400

    pass geolocation data to map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
-import Map from './components/Map/Map';
+import { useEffect, useState } from "react";
+import { getCurrentLatLng } from "./services/geolocation";
+import "./App.css";
+import Map from "./components/Map/Map";
 
-function App () {
-  return (
-    <div className='App'>
-      <Map />
-      <header className='App-header'>REACT WEATHER</header>
-    </div>
-  );
+function App() {
+	const [appData, setAppData] = useState({
+		lat: null,
+		lng: null,
+	});
+	async function getAppData() {
+		const { lat, lng } = await getCurrentLatLng();
+
+		setAppData({ lat, lng });
+		// we need weather data
+		// set component state to the received values
+	}
+	useEffect(() => {
+		// make your ajax request here or anything else you need on page load
+		getAppData();
+	}, []);
+	return (
+		<div className="App">
+			<Map lat={appData.lat} lng={appData.lng} />
+			<header className="App-header">REACT WEATHER</header>
+		</div>
+	);
 }
-
 export default App;
