af82c6a56d778e0899d631e16b72bb9d817ace0d
Abdisamad Hussein <78183395+abdi-hu@users.noreply.github.com>
2021-04-02 17:01:49 -0400

    implement openweatherapp api

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ function App() {
 	const [appData, setAppData] = useState({
 		lat: null,
 		lng: null,
+		temp: null,
+		icon: "",
 	});
 	async function getAppData() {
 		const { lat, lng } = await getCurrentLatLng();
 
-		setAppData({ lat, lng });
-		// we need weather data
-		// set component state to the received values
+		const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+		const API_KEY = "5b3c5a41e420b342a7d2e498f5e3fd82";
+		// ?lat=34.0475869&lon=-117.8985651&units=imperial&appid=
+		const weatherData = await fetch(
+			`${BASE_URL}?lat=${lat}&lon=${lng}&units=imperial&appid=${API_KEY}`
+		).then((res) => res.json());
+
+		setAppData({
+			lat,
+			lng,
+			temp: weatherData.main.temp,
+			icon: weatherData.weather[0].icon,
+		});
 	}
 	useEffect(() => {
 		// make your ajax request here or anything else you need on page load
@@ -22,7 +34,16 @@ function App() {
 	return (
 		<div className="App">
 			<Map lat={appData.lat} lng={appData.lng} />
-			<header className="App-header">REACT WEATHER</header>
+			<header className="App-header">
+				{appData.temp && <div>{appData.temp}&deg;</div>}
+				REACT WEATHER
+				{appData.icon && (
+					<img
+						src={`https://openweathermap.org/img/w/${appData.icon}.png`}
+						alt="Current Conditions"
+					/>
+				)}
+			</header>
 		</div>
 	);
 }
